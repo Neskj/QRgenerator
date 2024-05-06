72529274dc0299f3d0887fa7e673efd8fc6b12c4
Neskj <pronin.qq@gmail.com>
2024-05-06 06:43:54 +0300

    start project

---
+++ QRgenerator/src/main/java/neskj/QRgenerator/QRgeneratorApplication.java
@@ -0,0 +1,15 @@
+package neskj.QRgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QRgeneratorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(QRgeneratorApplication.class, args);
+		System.out.println("Hello from QRgenerator !!!");
+	}
+
+}
---
+++ QRgenerator/src/test/java/neskj/QRgenerator/QRgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package neskj.QRgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QRgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
